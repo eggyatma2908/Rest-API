fc15ab4b222ee7869d74565bdd95383f8c8dfffc
eggyatma2908 <eggyatmariansyah@gmail.com>
2021-02-02 22:28:53 +0700

    Completed V1

--- src/routers/users.js
+++ src/routers/users.js
@@ -6,7 +6,7 @@ const { verifyAccess } = require('../middlewares/auth')
 // const { cacheAllUsers} = require('../middlewares/redis')
 
 router
-  .get('/', getDataUsers)
+  .get('/', verifyAccess, getDataUsers)
   .get('/:id', verifyAccess, getDataUserById)
   .post('/', verifyAccess, insertDataUser)
   .post('/register', emailVerification, registerUser)
@@ -14,6 +14,6 @@ router
   .post('/:id', verifyAccess, editPhone)
   .patch('/balancesender/:id', verifyAccess, updateBalanceSender)
   .patch('/balancereceiver/:id', verifyAccess, updateBalanceReceiver)
-  .patch('/profile/:id', updateImage)
+  .patch('/profile/:id', verifyAccess, updateImage)
   .delete('/:id', verifyAccess, deleteDataUserById)
 module.exports = router
