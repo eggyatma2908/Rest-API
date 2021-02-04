ae8501619703a10da8ee33ee4f0096206f125b70
eggyatma2908 <eggyatmariansyah@gmail.com>
2021-02-04 08:03:20 +0700

    Update

--- src/helpers/emailVerification.js
+++ src/helpers/emailVerification.js
@@ -1,10 +1,8 @@
 var nodemailer = require('nodemailer')
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    ignoreTLS: false,
-    secure: false,
+    service: "gmail",
+    host: "smtp.gmail.com",
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
